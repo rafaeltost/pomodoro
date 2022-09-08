f684781cf9df29750b6cbb7c290dd4c60d2af4f9
Rafael <rafaeltost@gmail.com>
2022-09-08 01:29:57 -0300

    initial project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cronômetro
+
+Pequeno projeto em Flutter explorando Mobx como gerenciador de estado dos controles do temporizador e do tema.
+
+![cronometro](C:\Users\Rafael\Desktop\cronometro.jpg)
---
+++ lib/components/cronometro_botao.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CronometroBotao extends StatelessWidget {
+  final String texto;
+  final IconData icone;
+  final void Function()? click;
+
+  const CronometroBotao({
+    Key? key,
+    required this.texto,
+    required this.icone,
+    this.click,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.black,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 25,
+          vertical: 15,
+        ),
+        textStyle: const TextStyle(
+          fontSize: 20,
+        ),
+      ),
+      onPressed: click,
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(
+              icone,
+              size: 30,
+            ),
+          ),
+          Text(texto),
+        ],
+      ),
+    );
+  }
+}
